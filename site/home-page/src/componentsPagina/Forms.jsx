b444706481eaece5007e6f6728daf702d70fe7e8
joao-vitor-vlr <joao.rosa@bandtec.com.br>
2021-11-10 18:04:56 -0300

    integracao

--- site/home-page/src/componentsPagina/Forms.jsx
+++ site/home-page/src/componentsPagina/Forms.jsx
@@ -1,39 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import Footer from "../components/Footer";
 import NavbarSecundario from "../components/NavbarSecundario.jsx";
+import api from '../api';
 
+function Forms(props) {
 
-function Forms(props){
-    return(
-    <>
-    <NavbarSecundario/>
-        <div class="container4">
-            <div class="dadosCadastro">
-                <h4 class="tituloForm">{props.titulo}</h4>
-                <form action="" class="formClass">
-                    <label for=""> {props.campoUm}
-                        <input type="text"/>
-                    </label>
-                    <label for="">{props.campoDois}
-                        <input type="text"/>
-                    </label>
-                    <label for=""> {props.campoTres}
-                        <input type="text"/>
-                    </label>
-                    <label for=""> {props.campoQuatro}
-                        <input type="text"/>
-                    </label>
-                    <label for=""> {props.campoCinco}
-                        <input type="text"/>
-                    </label>
-                </form>
+    const [campo1, setCampo1] = useState("");
+    const [campo2, setCampo2] = useState("");
+    const [campo3, setCampo3] = useState("");
+    const [campo4, setCampo4] = useState("");
+    const [campo5, setCampo5] = useState("");
 
-                <button class="buttonCadastrar">Cadastrar</button>
+    function atleta(e) {
+        e.preventDefault();
+    
+        api.post("/atletas", {
+          cpf: campo5,
+          nomeAtleta: campo1,
+          email: campo2,
+          senha: campo3,
+          dataNasc: campo4,
+         
+        }).then((resposta) => {
+          if (resposta.status === 201) {
+            alert("aperte enter para se redirecionar");
+            // history.push('/atletas');
+          }
+        }).catch((erro) => {
+          console.log(erro);
+        })
+      }
+      
+    return (
+        <>
+            <NavbarSecundario />
+            <div class="container4">
+                <div class="dadosCadastro">
+                    <h4 class="tituloForm">{props.titulo}</h4>
+                    <form onSubmit={props.usuario} class="formClass">
+                        <label for=""> {props.campoUm}
+                            <input type="text" onChange={e => setCampo1(e.target.value)}/>
+                        </label>
+                        <label for="">{props.campoDois}
+                            <input type="text" onChange={e => setCampo2(e.target.value)}/>
+                        </label>
+                        <label for=""> {props.campoTres}
+                            <input type="text" onChange={e => setCampo3(e.target.value)}/>
+                        </label>
+                        <label for=""> {props.campoQuatro}
+                            <input type="text" onChange={e => setCampo4(e.target.value)}/>
+                        </label>
+                        <label for=""> {props.campoCinco}
+                            <input type="text" onChange={e => setCampo5(e.target.value)}/>
+                        </label>
+                        <button class="buttonCadastrar" type="submit">Cadastrar</button>
+                    </form>
+
+                    
+                </div>
+
             </div>
-
-        </div>
-    <Footer/>
-    </>
+            <Footer />
+        </>
     );
 }
 export default Forms;
