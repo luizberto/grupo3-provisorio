378df1cac22364208e11c07de77f0de0106e2bce
luizberto <70718303+luizberto@users.noreply.github.com>
2021-11-20 12:19:42 -0300

    Mudanças no horario do site

--- site/home-page/src/componentsPagina/DadosQuadra.jsx
+++ site/home-page/src/componentsPagina/DadosQuadra.jsx
@@ -1,50 +1,19 @@
 import React, {useEffect, useState} from "react";
-import fundoperfil from "../home-template-css/App/img/camp-nou-5798015_640 1 (1).png";
 import api from "../api";
 
 function DadosQuadra(props) {
-
-    const [horario, setHorario] = useState([]);
-
-    useEffect(() => {
-      async function pegaDados() {
-        const resposta = await api.get("/horarios/"+sessionStorage.getItem('idQuadra'));
-        if (resposta.status === 201) {
-          setHorario(resposta.data);
-        }
-
-        console.log(resposta.data);
-        alert(resposta.data);
-      }
-      pegaDados();
-    }, []);
-  
-  return (
+  return(
     <>
-      
-      <div class="box-info-quadra">
-        <span class="tituloCard">
-          <h1>descrição</h1>
-        </span>
-
         <p>Limite de pessoas:</p>
         <p>Materiais esportivos:</p>
         <p>Telefone:</p>
         <p>Endereço:</p>
         <p>Cep:</p>
-        <p>Descrição: {sessionStorage.getItem('descricao')}</p>
-      </div>
+        <p>Descrição: {sessionStorage.getItem('descricao')}</p>    
 
-      <div class="box-info-quadra">
-        <span class="tituloCard">
-          <h1>horarios</h1>
-          {horario.map((quadra) => (
-            <div>{quadra.fkQuadra}</div>
-          ))}
-        </span>
-      </div>
     </>
   );
+  
 }
 
 export default DadosQuadra;
