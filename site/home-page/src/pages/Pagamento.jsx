c3b51c03cd92c3281de06320b0db3c845466d8a3
luizberto <70718303+luizberto@users.noreply.github.com>
2021-11-22 00:45:41 -0300

    att da madrugada

--- site/home-page/src/pages/Pagamento.jsx
+++ site/home-page/src/pages/Pagamento.jsx
@@ -1,24 +1,10 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import NavbarSecundario from "../components/NavbarSecundario";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Pagamento(){
 
-    const[chekced, setchecked] = useState(false)
-    let pix = document.getElementById("pix")
-    let credito = document.getElementById("credito")
-    let presencial = document.getElementById("presencial")
-
-    function apaga(){
-        setchecked(!chekced)
-    }
-
-    const chekcedClass = chekced ? 'checked':'';
     
-
-  
-
     return(
         <>
         <NavbarSecundario/>
@@ -68,36 +54,18 @@ function Pagamento(){
        
         <h3>Formas de pagamento</h3>
 
-    <form action="" name="formulario">
-        <div class="cartoes">
 
-        <span className = "check" onClick = {apaga} >
-        <label id="presencial">Presencial</label>  
-        </span>
+    <form action="" name="formulario" class = "formPagamento">
 
-        <span className = "check" onClick = {apaga} >
-          <label id="credito">Credito</label>  
-        </span>
-
-        <span className = "check2" onClick = {apaga}>
-        <label id="pix"> Pix</label>
-         </span>
-
-        <div>
-            
- </div>
-        </div>
         <label for="">
-          Valor: <input type="text" id="campo" name="campo"/>
+          Valor: <input type="text" id="valor" name="campo"/>
         </label>
-      
         <label for="">
           Pagar: <input type="text"/>
         </label>
-
        
     </form>
-       <button onclick="chamarAviso()" class="avisin" >confirmar</button>
+       <button class="avisin" >confirmar</button>
     </div>
    
 </div>
