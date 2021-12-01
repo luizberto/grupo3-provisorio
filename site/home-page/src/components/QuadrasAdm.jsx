4afc988d73c7faec7d3385b33036d84234a55c18
joao-vitor-vlr <joao.rosa@bandtec.com.br>
2021-12-01 16:40:36 -0300

    att

--- site/home-page/src/components/QuadrasAdm.jsx
+++ site/home-page/src/components/QuadrasAdm.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 
-function QuadrasAdm(props){
-    return(
+function QuadrasAdm(props) {
+    return (
         <>
 
-                <div class="quadra">
-                    <h1>Quadra {props.key}</h1>
-                    <ul>
-                        <li>Nome da quadra:{props.nome}</li>
-                        <li>Descrição da quadra: <p>{props.descricao}</p></li>
-                        <li>Limite de ocupação:{props.ocupacao}</li>
-                    </ul>  
-                    <a href="">vizualizar&gt; </a>
-                </div>
+            <div class="quadra">
+                <h1>Quadra {props.key}</h1>
+                <ul>
+                    <li>Nome da quadra:{props.nome}</li>
+                    <li>Descrição da quadra: <p>{props.descricao}</p></li>
+                    <li>Limite de ocupação:{props.ocupacao}</li>
+                </ul>
+                <a href="">vizualizar&gt; </a>
+            </div>
 
 
         </>
     );
 }
+
 export default QuadrasAdm;
