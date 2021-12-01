70bdd1c2116a0b0165f4ce4eea981c38404816d7
joao-vitor-vlr <joao.rosa@bandtec.com.br>
2021-12-01 16:46:53 -0300

    arrumei uma merda

---
+++ site/home-page/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../home-template-css/img/image 22.png"
+
+function Footer(){
+    return(
+        <>
+        <footer class="footer">
+        <a class="logo" href="/"><img src={logo} height="40px" alt=""/></a>
+        <ul class="menu">
+            <a href=""><li>Ajuda</li></a> 
+            <a href=""><li>Contato</li></a> 
+            <a href=""><li>FAQ</li></a>
+            <a href=""><li>Politica de Privacidade</li></a> 
+        </ul>
+    </footer>
+    </>
+    );
+}
+export default Footer;
