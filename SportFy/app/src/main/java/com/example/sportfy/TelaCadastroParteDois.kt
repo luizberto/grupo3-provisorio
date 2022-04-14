9d52f8e5a0ada0763d1581b5c531055da5237635
Mateus Pereira <mateus.silva@sptech.school>
2022-04-13 21:59:55 -0300

    adicionando telas cadastro

---
+++ SportFy/app/src/main/java/com/example/sportfy/TelaCadastroParteDois.kt
@@ -0,0 +1,11 @@
+package com.example.sportfy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaCadastroParteDois : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_cadastro_parte_dois)
+    }
+}
